81f842dc1fb51fdec4b077338e635eff5ba29673
Arian Moharramzadeh <arian.moharramzadeh@campus.lmu.de>
2021-12-05 19:40:14 +0100

    hier noch formatierte ausgabe einbauen

--- handleResponse.c
+++ handleResponse.c
@@ -1,41 +1,43 @@
 #include "handleResponse.h"
 
 int step=1;
-
+//vieles auskommentierte noch vom alten Ansatz zur formatierten Ausgabe, kann helfen
+// bisher nur die "normalen" Kommandozeilennachrichten die vom Server empfangen wurden welche noch schön formatiert ausgegeben werden müssen
 char *handle(char *request){
     char *response;
-    char *print;
+    //char *print;
 
     if((response = malloc(BUFFERLENGTH*sizeof(char)))==NULL){
         //TODO: Fehlerbehandlung
     }
-
+    
+    /*
     if((print = malloc(BUFFERLENGTH*sizeof(char)))== NULL){
         //TODO: Fehlerbehandlung
-    }
+    }*/
 
     if (step==1 && match(request, "MNM Gameserver .+accepting connections"))
     {
-        char sver[10];
-        sprintf(sver, "%f", VERSION);
+        //char sver[10];
+        //sprintf(sver, "%f", VERSION);
         strcpy(response,"VERSION ");
-        strcat(response, sver);
-        //SChön formatierte Ausgabe
+        strcat(response, VERSION);
+        /*Schön formatierte Ausgabe
         strcpy(print, "The MNM server with version ");
         char *version = substring(request, 16,strlen(request)-22);      //Aus dem vom Server gesendeten String die Versionsnummer herausfiltern
         strcat(print, version);
-        strcat(print, " accepted the connection. ");
+        strcat(print, " accepted the connection. "); 
 
         if (version != NULL) {
             free(version);
-        }  
+        }  */
         step++;  
     } else if (step==2 && match(request, "Client version accepted - please send Game-ID to join"))
     {
         strcpy(response, "ID ");
         strcat(response, game_id);
         //Ausgabe
-        strcpy(print, "Client version accepted - please send Game-ID to join.");
+        //strcpy(print, "Client version accepted - please send Game-ID to join.");
 
         step++;
 
@@ -44,7 +46,7 @@ char *handle(char *request){
             free(response);
         }
         response = NULL;                                                //NULL-Pointer, da der Client hier nicht reagieren muss
-        //Ausgabe
+        /*Ausgabe
         strcpy(print, "Es wird das Spiel ");
         char *gamekind = substring(request, 9, strlen(request));
         strcat(print, gamekind);
@@ -53,17 +55,17 @@ char *handle(char *request){
         if (gamekind != NULL)
         {
             free(gamekind);
-        }
+        } */
 
         step++;
         
     }else if (step == 4 && match(request, ".+")){
-        char sint[12];
-        sprintf(sint, "%d", player_number);
+        //char sint[12];
+        //sprintf(sint, "%d", player_number);
         strcpy(response, "PLAYER ");
-        strcat(response, sint); 
+        strcat(response, player_number); 
 
-        //Ausgabe
+        /*Ausgabe
         strcpy(print, "Diese Partie trägt den Namen : \'");
         char *gamename = substring(request, 2, strlen(request));
         strcat(print, gamename);
@@ -72,7 +74,7 @@ char *handle(char *request){
         if (gamename != NULL)
         {
             free(gamename);
-        }
+        }*/
         step++;
 
     }else if(step == 5 && match(request, "YOU .+ .+")){
@@ -81,7 +83,7 @@ char *handle(char *request){
             free(response);
         }
         response=NULL;
-        //Ausgabe (hier auch die jeweiligen Werte im richtigen Format in der Struct ablegen (in Milestone 2))
+        /*Ausgabe (hier auch die jeweiligen Werte im richtigen Format in der Struct ablegen (in Milestone 2))
         strcpy(print, "You are the Player with the number ");
         char *playnum = substring(request, 5,7);                    //evtl. verzählt --> Testen und Zahlen ggf. anpassen
         strcat(print, playnum);
@@ -96,14 +98,14 @@ char *handle(char *request){
         if (playname != NULL)
         {
             free(playname);
-        }
+        } */
         step++;
     }else if (step == 6 && match(request, "TOTAL .+")){
         if (response != NULL){
             free(response);
         }
         response=NULL;
-
+        /*
         char *totalPlayer = substring(request, 6, strlen(request));
         int anz = atoi(totalPlayer);
         if(anz > 1){
@@ -116,7 +118,7 @@ char *handle(char *request){
         if (totalPlayer != NULL)
         {
             free(totalPlayer);
-        }
+        } */
         step++;
     }else if (match(request, "ENDPLAYERS") ){
         if(response!=NULL){
@@ -124,14 +126,19 @@ char *handle(char *request){
         }
         response=NULL;
 
-        strcpy(print, "The game starts. Have fun!");
+        printf("The game starts. Have fun!\n");
 
     }else if (step >= 7 && match(request, ".+ .+ .+")){
         if(response!=NULL){
             free(response);
         }
         response = NULL;
-
+        //hier erstmal provisorisch game-id frei gemacht, dadurch zum geradigen Zeitpunkt keine leaks möglich.
+        if (game_id!=NULL){
+            free(game_id);
+        }
+        game_id= NULL;
+        /*
         char *playNum = substring(request, 2, 4); //hier nochmal checken ob richtige Nummern
         char *playName = substring(request, 4, strlen(request)-1);
         char *status = substring(request, strlen(request)-1,strlen(request) );
@@ -157,9 +164,10 @@ char *handle(char *request){
         }
         if (status != NULL){
             free(status);
-        }
+        }*/
 
-    }else{                                                                        //Unbekannte Anfrage
+    }else{ 
+        /*                                                                       //Unbekannte Anfrage
         if(response!=NULL){
             free(response);
         }
@@ -167,16 +175,18 @@ char *handle(char *request){
         if(print!=NULL){
             free(print);
         }
-         print = NULL;
+         print = NULL;*/
+         strcpy(response,"Unknown request");
     }
 
     if(response != NULL){
         strcat(response, "\n");
-    }
+    } /*hier noch vllt kommentar
     if(print != NULL){
-        printf("Server: %s\n", print);
+        printf("S: %s\n", print);
         free(print);
-    }
+    } */
+    
 
     return  response;
     
