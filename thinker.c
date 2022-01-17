27156094eefb39eb5ed597419cd007e7fcbd0d34
Alexander Stadelmann <a.stadelmann@campus.lmu.de>
2022-01-17 19:25:47 +0100

    refactor of isWinningMove()

--- thinker.c
+++ thinker.c
@@ -1,5 +1,6 @@
 #include "header.h"
 #include "performConnection.h"
+#include "thinker.h"
 #include <time.h>
 #include <stdlib.h>
 
@@ -42,7 +43,7 @@ void insertNextMove() {
   strcat(nextMove, nextCoordinates);
   chooseNextOpponentPiece();
   char temp[4];
-  bool winningMove = isWinningMove(nextPiece, nextField, board);
+  bool winningMove = is_winning_move(nextPiece, nextField, board);
   if(winningMove) {
     puts("Yuhuuu");
   }
@@ -68,72 +69,92 @@ void chooseNextOpponentPiece() {
   }
 }
 
+/*is_winning_move() first uses "bitwise and" to check if four pieces in a line
+have a quality in common stored as a "1" in the binary representation of the piece.
+The lines can be horizonal, vertical or diagonal.
+If four pieces in a line do not share quality stored as "1", then the result
+of the four "bitwise and's" is zero.
 
-bool isWinningMove(int piece, int field, int board[42][4]) {
-  int res = piece;
+Also we have to check qualities stored as "0" in binary. 
+For that purpose we invert the binaries(the piece and the board)
+and proceed like before.
+*/
+bool is_winning_move(int piece, int field, int board[42][4]) {
+  //check for common qualities stored as "1"s
+  if(is_winning_move_helper(piece, field, board)) return true;
 
-  for(int k = 0; k < 2; k++) {
+  //invert binaries to check for common qualities stored as "0"s
+  piece ^= 15;
 
-    //invert bits for second run
-    if(k == 1) res = res^15;
-    
-
-    int column = field % 4;
-    
-
-    int row = field / 4;
-    
-
-
-  //vertical line
   for(int i = 0; i < 4; i++) {
+    for(int j = 0; j < 4; j++){
+      
+      if(board[i][j] != -1) {
+        board[i][j] ^= 15;
+      }
+    } 
+  }
+
+  //call helper function with inverted binaries
+  return is_winning_move_helper(piece, field, board);
+
+}
+bool is_winning_move_helper(int piece, int field, int board[42][4]) {
+    int res = piece;
+    int column = field % 4;
+    int row = field / 4;
+  
+  //check vertical lines
+  for(int i = 0; i < 4; i++) {
+
+    //skip the field where we want to position the piece
     if(i == row) continue;
+
+    //if a field is empty, then we know its not a winning move
     if(board[i][column] == -1) {
       res = 0;
       break;
     }
-    if(k == 0) {
-      res = res & board[i][column];
-    } else {
-      res = res & (board[i][column]^15);
-    }
-    
+      //use bitwise and
+      res &= board[i][column];
   }
-
+  //if the result is not zero we have a winner
   if(res != 0) {
     return true;
-  } else {
-    res = piece;
-    if(k == 1) res = res^15;
-  }
+  } 
+
+  //restore initial value of res
+  res = piece;
+  
 
 
-  //horizontal line
+  //check horizontal lines like above
   for(int j = 0; j < 4; j++) {
+
     if(j == column) continue;
+
     if(board[row][j] == -1) {
       res = 0;
       break;
     }
-    if(k == 0) {
-      res = res & board[row][j];
-    } else {
-      res = res & (board[row][j]^15);
-    }
+
+    res = res & board[row][j];
     
     
   }
-
+  
   if(res != 0) {
     return true;
-  } else {
-    res = piece;
-    if(k == 1) res = res^15;
-  }
+  } 
+
+  res = piece;
+    
 
   //diagonal from upper left corner to lower right corner
   for(int i = 0; i < 4; i++){
     for(int j = 0; j < 4; j++){
+      
+      //the sum of the indexes of the fields on this diagonal equals three
       if(i + j == 3) {
         if(i == row && j == column) continue;
 
@@ -141,54 +162,39 @@ bool isWinningMove(int piece, int field, int board[42][4]) {
           res = 0;
           break;
         } 
-        if(k == 0) {
-          res = res & board[i][j];
-        } else {
-          res = res & (board[i][j]^15);
-        }
         
+        res = res & board[i][j];
       }
     } 
   }
 
   if(res != 0) {
     return true;
-  } else {
-    res = piece;
-    if(k == 1) res = res^15;
-  }
+  } 
 
-  //diagonal line from upper right corner to lower left corner
-  
+  res = piece;
+    
+
+  //diagonal line from lower left corner to upper right corner
   for(int i = 0; i < 4; i++) {
     for(int j = 0; j < 4; j++) {
+      
+      //row number equals column number on this diagonal
       if(i == j) {
         if(board[i][j] == -1) {
           res = 0;
           break;
         }
-        if(k == 0) {
-          res = res & board[i][j];
-        } else {
-          res = res & (board[i][j]^15);
-        }    
+        
+        res = res & board[i][j];
+       
       }
     }
   }
   if(res != 0) {
     return true;
-  } else {
-    res = piece;
-    if(k == 1) res = res^15;
-  }
-    
-  
-  
-  }
-  
-  
+  } 
+
   return false;
 
 }
-
-
---
+++ thinker.h
@@ -0,0 +1,2 @@
+bool is_winning_move(int, int, int[42][4]);
+bool is_winning_move_helper(int, int, int[42][4]);
